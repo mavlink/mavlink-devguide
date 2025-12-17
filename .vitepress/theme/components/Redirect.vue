8cd1826974deb60e3535e85b13b6d6f6463b9a1d
Hamish Willee <hamishwillee@gmail.com>
2025-12-18 10:18:51 +1100

    Upgrade vitepress and add open in editor functionality

--- .vitepress/theme/components/Redirect.vue
+++ .vitepress/theme/components/Redirect.vue
@@ -1,19 +1,17 @@
-<template>
-</template>
-
+<template></template>
 
 <script>
-    /** Redirects to the given 'to' url, which is relative to the current location. Originates from https://github.com/vuejs/vuepress/issues/239#issuecomment-632567115 */
-    export default {
-        name: 'Redirect',
-        props: {
-            to: {
-                type: String,
-                required: true
-            }
+/** Redirects to the given 'to' url, which is relative to the current location. Originates from https://github.com/vuejs/vuepress/issues/239#issuecomment-632567115 */
+export default {
+    name: "Redirect",
+    props: {
+        to: {
+            type: String,
+            required: true,
         },
-        beforeMount() {
-            document.location.replace(this.to);
-        }
-    }
+    },
+    beforeMount() {
+        document.location.replace(this.to);
+    },
+};
 </script>
